e5f5205f77f3625955889725f668ec449cf49151
smile085 <ramdhanikhsan70@gmail.com>
2024-11-16 13:11:19 +0700

    Add files via upload

---
+++ san.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String firstName = "Tsubasa";
+         String lastName = "ojora";
+         System.out.println("nama pertama : "+firstName);
+         System.out.println("nama kedua : "+lastName);
+         System.out.println("panjang nama pertama : "+firstName.length());
+         System.out.println("panjang nama kedua : "+lastName.length());
+        
+    }
+}
